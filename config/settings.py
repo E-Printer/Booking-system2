84100c67d29c6c0077c12ad7d00222e7b6fbcbec
Edward Printer <edwardprxnter@gmail.com>
2024-12-23 11:20:06 +0000

    graphviz installed and used

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,8 @@ INSTALLED_APPS = [
     'django.contrib.sites',  
     'allauth',  
     'allauth.account', 
-    'allauth.socialaccount',  
+    'allauth.socialaccount',
+    'django_extensions', 
 ]
 
 AUTHENTICATION_BACKENDS = [
@@ -172,3 +173,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPH_MODELS = {
+    'all_applications': True,
+    'group_models': True,
+}
